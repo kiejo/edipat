7f9db6cba14ee4a79990f559d02a88e5a7eb288d
kiejo <jonathan.kienzle@gmail.com>
2013-10-24 23:35:19 +0200

    added ast spec for editor

---
+++ adv_ast.js
@@ -0,0 +1,86 @@
+mylang features:
+
+def_var
+def_fn
+def_fna
+call_fn
+atom
+list
+arr
+obj
+str
+num
+
+
+
+(defn tail xs
+	(.slice xs 1))
+
+{
+	t: 'def_fn',
+	name: { t: 'atom', name: 'tail' },
+	args: [ { t: 'atom', name: 'xs' } ],
+	body: {
+		t: 'call_fn',
+		name: { t: 'atom', name: '.slice' },
+		args: [ { t: 'atom', name: 'xs' }, { t: 'num', val: 1} ]
+	}
+}
+
+
+(defn map f xs
+	(if (empty xs)
+		[]
+		(cons (f (head xs)) (map f (tail xs)))))
+
+{
+	t: 'def_fn',
+	name: { t: 'atom', name: 'map' },
+	args: [ { t: 'atom', name: 'f' }, { t: 'atom', name: 'xs' } ],
+	body: {
+		t: 'call_fn',
+		name: { t: 'atom', name: 'if' },
+		args: [
+			{
+				t: 'call_fn'
+				name: { t: 'atom' name: 'empty' }
+				args: { t: 'atom' name: 'xs' }
+			},
+			{ t: 'arr', els: [] }
+			{
+				t: 'call_fn'
+				name: { t: 'atom' name: 'cons' }
+				args: [
+					{
+						t: 'call_fn'
+						name: { t: 'atom' name: 'f' }
+						args: [
+							t: 'call_fn'
+							name: { t: 'atom' name: 'head' }
+							args: [
+								{ t: 'atom' name: 'head' }
+							]
+						]	
+					}
+					{
+						t: 'call_fn'
+						name: { t: 'atom' name: 'map' }
+						args: [
+							{
+								t: 'atom' name: 'f'
+							}
+							{
+								t: 'call_fn'
+								name: { t: 'atom' name: 'tail' }
+								args: [
+									{ t: 'atom' name: 'xs'}
+								]
+							}
+						]
+					}
+
+				]
+			}
+		]
+	}
+}
